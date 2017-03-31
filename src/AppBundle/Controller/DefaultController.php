3f88cc365ac594a28aac5efabd8a04ce0bf3a175
Ivan PRUNIER <ivan.prunier7@gmail.com>
2017-03-31 13:12:12 +0200

    Adds first graph + parsing for browsers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,50 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $rawData = file_get_contents(
+            'https://opendata.paris.fr/api/records/1.0/search/?dataset=utilisations_mensuelles_des_hotspots_paris_wi-fi&rows=10&facet=browser'
+        );
+        $jsonData = json_decode($rawData, true);
+        $browsers = $this->browserShares($jsonData);
+
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'browsers' => $browsers,
+            ]
+        );
+    }
+
+    private function browserShares($data)
+    {
+        $browsers = $data['facet_groups'][0]['facets'];
+
+        // Get the total number of values
+        $total = 0;
+        foreach ($browsers as $browser) {
+            $total += $browser['count'];
+        }
+
+        // Get the main browsers and categorizes the others as 'other'
+        $percentageLimit = $total * 0.02;
+        $filteredBrowsers = [];
+        $filteredBrowsers[] = [
+            'name' => 'Others',
+            'count' => 0
+        ];
+        foreach ($browsers as $browser) {
+            if ($browser['name'] === 'Other' || $browser['name'] === 'NULL') {
+                $filteredBrowsers[0]['count'] += $browser['count'];
+            } elseif ($browser['count'] >= $percentageLimit) {
+                $filteredBrowsers[] = [
+                    'name' => $browser['name'],
+                    'count' => $browser['count']
+                ];
+            } else {
+                $filteredBrowsers[0]['count'] += $browser['count'];
+            }
+        }
+
+        return $filteredBrowsers;
     }
 }
